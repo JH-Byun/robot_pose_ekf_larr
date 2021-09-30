c644ae0a5e8abcfd3a9158f472efda275bdfdc4e
JH-Byun <quswjdgus97@gmail.com>
2021-09-30 19:14:57 +0900

    2021-09-30-19-14

--- src/odom_estimation_node.cpp
+++ src/odom_estimation_node.cpp
@@ -109,14 +109,18 @@ namespace estimation
     // subscribe to odom messages
     if (odom_used_){
       ROS_DEBUG("Odom sensor can be used");
-      odom_sub_ = nh.subscribe("odom", 10, &OdomEstimationNode::odomCallback, this);
+      // odom_sub_ = nh.subscribe("odom", 10, &OdomEstimationNode::odomCallback, this);
+      // JH - ADDED
+      odom_sub_ = nh.subscribe("mavros/vision_pose/pose", 10, &OdomEstimationNode::odomCallback, this);
     }
     else ROS_DEBUG("Odom sensor will NOT be used");
 
     // subscribe to imu messages
     if (imu_used_){
       ROS_DEBUG("Imu sensor can be used");
-      imu_sub_ = nh.subscribe("imu_data", 10,  &OdomEstimationNode::imuCallback, this);
+      // imu_sub_ = nh.subscribe("imu_data", 10,  &OdomEstimationNode::imuCallback, this);
+      // JH - ADDED
+      imu_sub_ = nh.subscribe("mavros/imu/data", 10, &OdomEstimationNode::imuCallback, this);
     }
     else ROS_DEBUG("Imu sensor will NOT be used");
 
